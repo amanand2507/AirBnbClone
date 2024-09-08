8fd0cb2dd14006367de83e55ed43212cfa3bd115
amanand2507 <amanand257@gmail.com>
2024-09-08 23:17:11 +0530

    env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,21 @@ import { PlaceModule } from './place/place.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the config module available globally
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'), // Load MONGODB_URI from .env
+      }),
+      inject: [ConfigService],
+    }),
     PlaceModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/airbnb'),
     UsersModule,
     AuthModule,
   ],
